b8dd1f5b101dc64c482f134e95cb99c2727c0747
Sanyee88 <k.sanyi888@gmail.com>
2021-10-10 12:49:52 +0200

    Client use SoftDeletes and ActiveScope global scope

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use App\Scopes\ActiveScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope(new ActiveScope);
+    }
 }
