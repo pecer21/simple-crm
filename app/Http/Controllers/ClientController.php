e52be8346f915390dc84ef6d188fa9822974db9b
Sanyee88 <k.sanyi888@gmail.com>
2021-10-10 23:17:14 +0200

    client destroy endpoint

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -81,6 +81,7 @@ class ClientController extends Controller
      */
     public function destroy(Client $client)
     {
-        //
+        $client->delete();
+        return redirect(route('clients.index'));
     }
 }
