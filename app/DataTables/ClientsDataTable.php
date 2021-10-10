e8ec9d71577553ace906fefcbfd87856d765b671
Sanyee88 <k.sanyi888@gmail.com>
2021-10-10 22:40:17 +0200

    create ClientsDataTable

---
+++ app/DataTables/ClientsDataTable.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\Client;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class ClientsDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query)
+            ->editColumn('action', function ($model) {
+                return view('components.datatables.buttons', [
+                    'edit_url'  => route('clients.edit', $model),
+                    'delete_url'  => route('clients.destroy', $model)
+                ]);
+
+            });
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\Models\Client $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(Client $model)
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->setTableId('clients-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->dom('Bfrtip')
+                    ->orderBy(1)
+                    ->buttons(
+                        Button::make('create')->action("window.location = '".route('clients.create')."';")
+                    );
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            Column::make('name'),
+            Column::make('vat'),
+            Column::make('address'),
+            Column::computed('action')
+              ->exportable(false)
+              ->printable(false)
+              ->addClass('text-center'),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'Clients_' . date('YmdHis');
+    }
+}
