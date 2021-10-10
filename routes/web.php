a451b86276398563c20c37616468fff48e800664
Sanyee88 <k.sanyi888@gmail.com>
2021-10-10 13:23:27 +0200

    set clients and dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
+Route::get('/', [DashboardController::class, 'index']);
+Route::resource('/clients', ClientController::class);
 
-Route::resource('/', ClientController::class);
