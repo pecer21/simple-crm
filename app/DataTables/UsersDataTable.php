953ba56e56caa85748bc65db5e2b7a2b04c337e8
Sanyee88 <k.sanyi888@gmail.com>
2021-10-16 23:37:15 +0200

    add UsersDataTable

---
+++ app/DataTables/UsersDataTable.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\User;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class UsersDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query);
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \User $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(User $model)
+    {
+        $query= $model::query()->with('roles');
+        if ($this->request()->has('deleted') && $this->request()->get('deleted') == true) {
+            $query->onlyTrashed();
+        }
+        return $query;
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->setTableId('usersdatatable-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->dom('frtip')
+                    ->orderBy(1);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            Column::make('name'),
+            Column::make('email'),
+            Column::make('roles[0].name')->title(__('datatable.column.user.role')),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'Users_' . date('YmdHis');
+    }
+}
