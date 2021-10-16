5eb7749db76f767c815290e5f3240d52d61b8c8b
Sanyee88 <k.sanyi888@gmail.com>
2021-10-16 12:15:49 +0200

    add auth and client routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index']);
-Route::resource('/clients', ClientController::class);
+Auth::routes();
 
+Route::middleware('auth')->group(function () {
+    Route::get('/', [DashboardController::class, 'index']);
+    Route::resource('/clients', ClientController::class);
+});
